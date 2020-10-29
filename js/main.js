e7ab10246174517f8a77bba3ac8537e260f574d6
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-10-29 19:41:34 +0200

    Make main_block && change header js logic

--- js/main.js
+++ js/main.js
@@ -5,27 +5,23 @@ let logo_layout = `<h1>Japanese Shop</h1>`;
 
 $(window).on('scroll', () => {
     if(show_2){
-        if($(window).scrollTop() > 20){
-            setTimeout(() => {
-                $(".header").css({position: "fixed"});
+        if($(window).scrollTop() > 150){
+            $(".header").css({animation: "hideHeader 0.5s"});
+            $(".header").on('animationend', () => {
                 $(".header").css({height: "150px"});
                 logo.html(`<img src="img/logo_6.png" style="height:150px">`)
 
                 $(".header").css({animation: "showHeader 0.5s"});
                 $(".header").on('animationend', () => {
-                    $(".header").css({position: "fixed"});
-                    $(".header").css({height: "150px"});
                     $(".header").css({animation: "none"});
-                    $(".header").css({top: "0"});
-                    logo.html(`<img src="img/logo_6.png" style="height:150px">`)
-                    show_2 = false;
                     show_1 = true;
+                    show_2 = false;
                 })
-            }, 500)
+            })
         }
     }
     if(show_1){
-        if($(window).scrollTop() < 20){
+        if($(window).scrollTop() < 150){
             $(".header").css({animation: "hideHeader 0.5s"});
             $(".header").on('animationend', () => {
                 logo.html(logo_layout)
@@ -33,13 +29,22 @@ $(window).on('scroll', () => {
                 $(".header").css({animation: "showHeader 0.5s"});
                 $(".header").on('animationend', () => {
                     $(".header").css({animation: "none"});
-                    $(".header").css({position: "relative"});
                     show_2 = true;
                     show_1 = false;
+                    
                 })
                 
             })
             
         }
     }
+})
+$(document).ready(() => {
+
+    $('.slick-slider').slick({
+        "setting-name": 'setting-value',
+        arrows: true,
+        nextArrow: $("#next"),
+        prevArrow: $("#prev")
+    })
 })
